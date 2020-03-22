18b4a5555d83433e028413385f3e7d2dbbba7fe5
liulei <liulei1@365trade.com.cn>
2020-03-22 19:29:26 +0800

    add: 增加代码生成配置文件

--- src/main/java/CodeGenerator.java
+++ src/main/java/CodeGenerator.java
@@ -1,6 +1,7 @@
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
@@ -13,30 +14,9 @@ import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import java.util.*;
 
 public class CodeGenerator {
-	/**
-	 * 包名
-	 */
-	private static final String PACKAGE_NAME = "com";
-	/**
-	 * 模块名称
-	 */
-	private static final String MODULE_NAME = "demo";
-	/**
-	 * 输出文件的路径
-	 */
-	private static final String OUT_PATH = "E:\\code";
-	/**
-	 * 代码生成者
-	 */
-	private static final String AUTHOR = "liulei";
 
-	/**
-	 * JDBC相关配置
-	 */
-	private static final String DRIVER = "com.mysql.jdbc.Driver";
-	private static final String URL = "jdbc:mysql://localhost:3336/demo?useUnicode=true&characterEncoding=UTF-8";
-	private static final String USER_NAME = "root";
-	private static final String PASSWORD = "123456";
+	// 输出表名称,如果要生成全部表则为空字符串
+	private static final String TABLE_NAME = "temp_meta";//导出表名称
 
 	/**
 	 * <p>
@@ -52,108 +32,122 @@ public class CodeGenerator {
 		// 虽然像Mysql数据库有自动更新几只，但像ORACLE的数据库就没有了，
 		// 使用公共字段填充功能，就可以实现，自动按场景更新了。
 		// 如下是配置
-		TableFill createField = new TableFill("version", FieldFill.INSERT);
+		TableFill createField = new TableFill("strategyConfig", FieldFill.INSERT);
 		tableFillList.add(createField);
 
+		final ResourceBundle prop = ResourceBundle.getBundle("config");
+
 		// 代码生成器
-		AutoGenerator mpg = new AutoGenerator().setGlobalConfig(
-				// 全局配置
-				new GlobalConfig().setOutputDir(OUT_PATH)// 输出目录
-						.setFileOverride(true)// 是否覆盖文件
-						.setActiveRecord(true)// 开启 activeRecord 模式
-						.setEnableCache(false)// XML 二级缓存
-						.setBaseResultMap(false)// XML ResultMap
-						.setBaseColumnList(true)// XML columList
-						.setAuthor(AUTHOR)
-						.setSwagger2(false)
-						// 自定义文件命名，注意 %s 会自动填充表实体属性！
-						.setXmlName("%sMapper").setDateType(DateType.ONLY_DATE).setIdType(IdType.ID_WORKER)
-		// .setServiceName("MP%sService")
-		// .setServiceImplName("%sServiceDiy")
-		// .setControllerName("%sAction")
-		).setDataSource(
-				// 数据源配置
-				new DataSourceConfig().setDbType(DbType.MYSQL)// 数据库类型
-						.setTypeConvert(new MySqlTypeConvert()).setDriverName(DRIVER).setUsername(USER_NAME)
-						.setPassword(PASSWORD).setUrl(URL))
-				.setStrategy(
-						// 策略配置
-						new StrategyConfig()
-								// .setCapitalMode(true)// 全局大写命名
-								// .setTablePrefix(new String[]{"unionpay_"})//
-								// 此处可以修改为您的表前缀
-								.setNaming(NamingStrategy.underline_to_camel)// 表名生成策略
-								.setInclude(new String[] {"base_lump_preliminaries_item"}) // 需要生成的表
-								// .setExclude(new String[]{"test"}) // 排除生成的表
-								// 自定义实体，公共字段
-								.setSuperEntityColumns(new String[]{"gmt_create","gmt_modified"})
-								.setTableFillList(tableFillList)
-								// 自定义实体父类
-								// .setSuperEntityClass("com.baomidou.demo.base.BsBaseEntity")
-								// // 自定义 mapper 父类
-								// .setSuperMapperClass("com.baomidou.demo.base.BsBaseMapper")
-								// // 自定义 service 父类
-								// .setSuperServiceClass("com.baomidou.demo.base.BsBaseService")
-								// // 自定义 service 实现类父类
-								// .setSuperServiceImplClass("com.baomidou.demo.base.BsBaseServiceImpl")
-								// 自定义 controller 父类
-								// .setSuperControllerClass("com.baomidou.demo.TestController")
-								// 【实体】是否生成字段常量（默认 false）
-								// public static final String ID = "test_id";
-								.setEntityColumnConstant(true)
-								// 【实体】是否为构建者模型（默认 false）
-								// public User setName(String name) {this.name =
-								// name; return this;}
-								.setEntityBuilderModel(true)
-								// 【实体】是否为lombok模型（默认 false）<a
-								// href="https://projectlombok.org/">document</a>
-								.setEntityLombokModel(false)
-								.setRestControllerStyle(false)
-								.setVersionFieldName("version")
-				// Boolean类型字段是否移除is前缀处理
-				// .setEntityBooleanColumnRemoveIsPrefix(true)
-				// .setRestControllerStyle(true)
-				// .setControllerMappingHyphenStyle(true)
-				)
-				.setPackageInfo(
-						// 包配置
-						new PackageConfig().setModuleName(MODULE_NAME).setParent(PACKAGE_NAME)// 自定义包路径
-//								.setController("controller")// 这里是控制器包名，默认 web
-//								.setMapper("dao")
-								.setXml("mapper")
+		GlobalConfig globalConfig = new GlobalConfig();
+		globalConfig.setOutputDir(prop.getString("outputDir"));// 输出目录
+		globalConfig.setFileOverride(true);// 是否覆盖文件
+		globalConfig.setActiveRecord(true);// 开启 activeRecord 模式
+		globalConfig.setEnableCache(false);// XML 二级缓存
+		globalConfig.setBaseResultMap(false);// XML ResultMap
+		globalConfig.setBaseColumnList(true);// XML columList
+		globalConfig.setAuthor(prop.getString("author"));
+		globalConfig.setSwagger2(false);
+		// 自定义文件命名，注意 %s 会自动填充表实体属性！
+		globalConfig.setXmlName("%sMapper");
+		globalConfig.setDateType(DateType.ONLY_DATE);
+		globalConfig.setIdType(IdType.ID_WORKER);
+		//globalConfig.setServiceName("MP%sService")
+		//globalConfig.setServiceImplName("%sServiceDiy")
+		//globalConfig.setControllerName("%sAction")
 
-				).setCfg(
-						// 注入自定义配置，可以在 VM 中使用 cfg.abc 设置的值
-						new InjectionConfig() {
-							@Override
-							public void initMap() {
-								Map<String, Object> map = new HashMap<String, Object>();
-								map.put("abc", this.getConfig().getGlobalConfig().getAuthor() + "-mp");
-								this.setMap(map);
-							}
-						}.setFileOutConfigList(
-								Collections.<FileOutConfig>singletonList(new FileOutConfig("/templates/mapper.xml.vm") {
-									// 自定义输出文件目录
-									@Override
-									public String outputFile(TableInfo tableInfo) {
-										return OUT_PATH + "/xml/" + tableInfo.getEntityName() + "Mapper.xml";
-									}
-								})))
-				.setTemplate(
-						// 关闭默认 xml 生成，调整生成 至 根目录
-						new TemplateConfig().setXml(null)
-		// 自定义模板配置，模板可以参考源码 /mybatis-plus/src/main/resources/template 使用 copy
+		// 数据源配置
+		DataSourceConfig dataSourceConfig = new DataSourceConfig();
+		dataSourceConfig.setDbType(DbType.MYSQL);
+		dataSourceConfig.setTypeConvert(new MySqlTypeConvert());
+		dataSourceConfig.setDriverName(prop.getString("driver"));
+		dataSourceConfig.setUrl(prop.getString("url"));
+		dataSourceConfig.setUsername(prop.getString("userName"));
+		dataSourceConfig.setPassword(prop.getString("password"));
+
+		// 策略配置
+		StrategyConfig strategyConfig = new StrategyConfig();
+		// strategyConfig.setCapitalMode(true);// 全局大写命名
+		// strategyConfig.setTablePrefix(new String[]{"unionpay_"});//
+		// 此处可以修改为您的表前缀
+		strategyConfig.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
+		if(StringUtils.isNotEmpty(TABLE_NAME)) {
+			strategyConfig.setInclude(new String[]{TABLE_NAME}); // 需要生成的表
+		}
+		// .setExclude(new String[]{"test"}) // 排除生成的表
+		// 自定义实体，公共字段
+		strategyConfig.setSuperEntityColumns(new String[]{"gmt_create", "gmt_modified"});
+		strategyConfig.setTableFillList(tableFillList);
+		// 自定义实体父类
+		// strategyConfig.setSuperEntityClass("com.baomidou.demo.base.BsBaseEntity");
+		// // 自定义 mapper 父类
+		// strategyConfig.setSuperMapperClass("com.baomidou.demo.base.BsBaseMapper");
+		// // 自定义 service 父类
+		// strategyConfig.setSuperServiceClass("com.baomidou.demo.base.BsBaseService");
+		// // 自定义 service 实现类父类
+		// strategyConfig.setSuperServiceImplClass("com.baomidou.demo.base.BsBaseServiceImpl");
+		// 自定义 controller 父类
+		// strategyConfig.setSuperControllerClass("com.baomidou.demo.TestController");
+		// 【实体】是否生成字段常量（默认 false）
+		// public static final String ID = "test_id";
+		strategyConfig.setEntityColumnConstant(true);
+		// 【实体】是否为构建者模型（默认 false）
+		// public User setName(String name) {this.name =
+		// name; return this;}
+		strategyConfig.setEntityBuilderModel(true);
+		// 【实体】是否为lombok模型（默认 false）<a
+		// href="https://projectlombok.org/">document</a>
+		strategyConfig.setEntityLombokModel(false);
+		strategyConfig.setRestControllerStyle(false);
+		strategyConfig.setVersionFieldName("strategyConfig");
+		// Boolean类型字段是否移除is前缀处理
+		//strategyConfig.setEntityBooleanColumnRemoveIsPrefix(true);
+		//strategyConfig.setRestControllerStyle(true);
+		//strategyConfig.setControllerMappingHyphenStyle(true);
+
+		// 包配置
+		PackageConfig packageConfig = new PackageConfig();
+		packageConfig.setParent(prop.getString("parent"));
+		packageConfig.setModuleName(prop.getString("moduleName"));
+//		packageConfig.setController("controller");// 这里是控制器包名，默认 web
+//		packageConfig.setMapper("dao");
+		packageConfig.setXml("mapper");
+
+		// 注入自定义配置，可以在 VM 中使用 cfg.abc 设置的值
+		InjectionConfig customConfig = new InjectionConfig() {
+			@Override
+			public void initMap() {
+				Map<String, Object> map = new HashMap<String, Object>();
+				map.put("abc", this.getConfig().getGlobalConfig().getAuthor() + "-mp");
+				this.setMap(map);
+			}
+		}.setFileOutConfigList(
+				Collections.<FileOutConfig>singletonList(new FileOutConfig("/templates/mapper.xml.vm") {
+					// 自定义输出文件目录
+					@Override
+					public String outputFile(TableInfo tableInfo) {
+						return prop.getString("outputDir") + "/xml/" + tableInfo.getEntityName() + "Mapper.xml";
+					}
+				}));
+
+		// 自定义模板配置，用于设置生成模板的文件,模板可以参考源码 /mybatis-plus/src/main/resources/template 使用 copy
 		// 至您项目 src/main/resources/template 目录下，模板名称也可自定义如下配置：
-		// .setController("...");
-		// .setEntity("...");
-		// .setMapper("...");
-		// .setXml("...");
-		// .setService("...");
-		// .setServiceImpl("...");
-		);
+		// 关闭默认 xml 生成，调整生成 至 根目录
+		TemplateConfig template = new TemplateConfig();
+		template.setXml(null);
+		template.setController(null);
+		template.setService(null);
+		template.setServiceImpl(null);
+		template.setMapper(null);
+		// template.setEntity("...");
 
-		// 执行生成
-		mpg.execute();
+		AutoGenerator generator = new AutoGenerator();
+		generator.setGlobalConfig(globalConfig);
+		generator.setDataSource(dataSourceConfig);
+		generator.setStrategy(strategyConfig);
+		generator.setPackageInfo(packageConfig);
+		generator.setCfg(customConfig);
+		generator.setTemplate(template);
+		generator.execute();
 	}
 
 }
